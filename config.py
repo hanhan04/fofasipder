41515d272a8f996842e06192aa97cc580c207ce2
hanhan04 <110645013+hanhan04@users.noreply.github.com>
2023-11-01 14:24:11 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ StopPage=""
 StartPage=""
 TimeSleep =""
 FileName  =""
-cookie="fofa_token=eyJhbGciOiJIUzUxMiIsImtpZCI6Ik5XWTVZakF4TVRkalltSTJNRFZsWXpRM05EWXdaakF3TURVMlkyWTNZemd3TUdRd1pUTmpZUT09IiwidHlwIjoiSldUIn0.eyJpZCI6MTY0NDE4LCJtaWQiOjEwMDA5NTc2OSwidXNlcm5hbWUiOiJnZXTkuIDlj6rmhqjmhqgiLCJleHAiOjE2OTg5ODk4MzV9.aKxrLnQ7kIcE2gpxM6YmYrEEFaJGZEN9_uYOwNptGUBzqv-idt8AZpm4VOfSeKrGMBn7-iC3u4ieh5ojP7so3g"
+cookie =""
 
 headers = {
         "Connection": "keep-alive",
